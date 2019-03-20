2ad2de949bf1b4ba5723a7a3882d64d23ff540bb
lifeng <lifeng@codeaurora.org>
2019-03-20 18:05:26 +0800

    prima: Fix possible buffer overflow in sirConvertAddtsRsp2Struct
    propagation from qcacld-3.0 to qcacld-2.0
    Further adapted from qcacld-2.0 to prima.
    
    In the function sirConvertAddtsRsp2Struct, iterator j is
    assigned with the value pAddTs->numTclas + addts.num_WMMTCLAS.
    The j value is used as the index to the array pAddTs->tclasInfo.
    Maximum limit on  pAddTs->tclasInfo entries is 2. So when the
    value of j exceeds 2, then a possible buffer overflow could
    occur.
    
    Validate the value of j against SIR_MAC_TCLASIE_MAXNUM(2).
    
    Issue: SEC-1907
    Change-Id: Icc723380ed4ccd51c729194d509e288be0e0712c
    CRs-Fixed: 2419829
    Bug: 128798879
    Bug: 123533258
    Signed-off-by: Srinivas Girigowda <quic_sgirigow@quicinc.com>
    (adapted from commit a1b88db1e50e0588737c41acd6a52468954d24b9)

--- drivers/staging/prima/CORE/SYS/legacy/src/utils/src/parserApi.c
+++ drivers/staging/prima/CORE/SYS/legacy/src/utils/src/parserApi.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2015 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2015,2019 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -3922,7 +3922,7 @@ sirConvertAddtsReq2Struct(tpAniSirGlobal    pMac,
         if ( addts.num_WMMTCLAS )
         {
             j = (tANI_U8)(pAddTs->numTclas + addts.num_WMMTCLAS);
-            if ( SIR_MAC_TCLASIE_MAXNUM > j ) j = SIR_MAC_TCLASIE_MAXNUM;
+            if ( SIR_MAC_TCLASIE_MAXNUM < j ) j = SIR_MAC_TCLASIE_MAXNUM;
 
             for ( i = pAddTs->numTclas; i < j; ++i )
             {
@@ -4104,7 +4104,7 @@ sirConvertAddtsRsp2Struct(tpAniSirGlobal    pMac,
         if ( addts.num_WMMTCLAS )
         {
             j = (tANI_U8)(pAddTs->numTclas + addts.num_WMMTCLAS);
-            if ( SIR_MAC_TCLASIE_MAXNUM > j ) j = SIR_MAC_TCLASIE_MAXNUM;
+            if ( SIR_MAC_TCLASIE_MAXNUM < j ) j = SIR_MAC_TCLASIE_MAXNUM;
 
             for ( i = pAddTs->numTclas; i < j; ++i )
             {
