d6884b27c03acd19942922d68c70a3d7b57074d2
Karsten Tausche <karsten@fairphone.com>
2020-06-26 16:49:58 +0200

    Fixup: msm: crypto: Fix integer over flow check in qcedev driver
    The previous patch work was not complete apperently. Add the missing
    changed line. Also removes some duplicated define that was introduced by
    different upstream patches.
    
    Issue: FPII-2407
    Issue: FP2P-319
    Change-Id: I3fb9157cb253ac6cf38f3a7d0683479ccf76635f

--- drivers/crypto/msm/qcedev.c
+++ drivers/crypto/msm/qcedev.c
@@ -41,7 +41,6 @@
 
 #define CACHE_LINE_SIZE 32
 #define CE_SHA_BLOCK_SIZE SHA256_BLOCK_SIZE
-#define U32_MAX		((u32)~0U)
 
 /* are FIPS integrity tests done ?? */
 bool is_fips_qcedev_integritytest_done;
@@ -1598,7 +1597,7 @@ static int qcedev_check_cipher_params(struct qcedev_cipher_op_req *req,
 	}
 	/* Check for sum of all dst length is equal to data_len  */
 	for (i = 0, total = 0; i < req->entries; i++) {
-		if (req->vbuf.dst[i].len >= ULONG_MAX - total) {
+		if (req->vbuf.dst[i].len >= U32_MAX - total) {
 			pr_err("%s: Integer overflow on total req dst vbuf length\n",
 				__func__);
 			goto error;
